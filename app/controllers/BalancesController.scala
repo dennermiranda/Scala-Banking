b29dfab7a60a8cc073d4017938f9510ad39cd8fc
Dener Miranda <denner.miranda@gmail.com>
2017-04-18 13:58:36 -0300

    Adding controllers and Routes

---
+++ app/controllers/BalancesController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api._
+import play.api.mvc._
+
+/**
+  * Created by Dener on 18/04/2017.
+  */
+
+@Singleton
+class BalancesController @Inject() extends Controller{
+
+  def deposit = Action{ request =>
+    Ok
+  }
+
+  def purchase = Action{ request =>
+    Ok
+  }
+
+  def withdrawal = Action{ request =>
+    Ok
+  }
+}
---
+++ app/controllers/BalancesDAO.scala
@@ -0,0 +1,8 @@
+package controllers
+
+/**
+  * Created by Dener on 18/04/2017.
+  */
+class BalancesDAO {
+
+}
