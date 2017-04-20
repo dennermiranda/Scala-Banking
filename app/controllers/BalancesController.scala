a54d2cc748006ee8f11ea79e3ab3f892bba49796
Dener Miranda <denner.miranda@gmail.com>
2017-04-20 13:46:46 -0300

    Adding action/post methods

--- app/controllers/BalancesController.scala
+++ app/controllers/BalancesController.scala
@@ -1,7 +1,10 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+
+import models._
 import play.api._
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc._
 
 /**
@@ -11,16 +14,42 @@ import play.api.mvc._
 @Singleton
 class BalancesController @Inject() (dao: BalancesDAO) extends Controller{
 
-  def deposit = Action{ request =>
-
-    Ok
+  def deposit = Action(BodyParsers.parse.json){ request =>
+    val depositResult = request.body.validate[Deposit]
+    depositResult.fold(
+      errors =>{
+        BadRequest((Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
+      },
+      deposit =>{
+        dao.addOperation(deposit)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+deposit.accountNumber+"' saved.") ))
+      }
+    )
   }
 
-  def purchase = Action{ request =>
-    Ok
+  def purchase = Action(BodyParsers.parse.json){ request =>
+    val purchaseResult = request.body.validate[Purchase]
+    purchaseResult.fold(
+      errors =>{
+        BadRequest((Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
+      },
+      purchase =>{
+        dao.addOperation(purchase)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+purchase.accountNumber+"' saved.") ))
+      }
+    )
   }
 
-  def withdrawal = Action{ request =>
-    Ok
+  def withdrawal = Action(BodyParsers.parse.json){ request =>
+    val withdrawalResult = request.body.validate[Withdrawal]
+    withdrawalResult.fold(
+      errors =>{
+        BadRequest((Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
+      },
+      withdrawal =>{
+        dao.addOperation(withdrawal)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+withdrawal.accountNumber+"' saved.") ))
+      }
+    )
   }
 }
