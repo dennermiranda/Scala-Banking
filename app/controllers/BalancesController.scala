11fb81d24d1fa2533b13974a23ac398f5f54f2dc
Dener Miranda <denner.miranda@gmail.com>
2017-04-21 10:56:18 -0300

    Adds getbalance

--- app/controllers/BalancesController.scala
+++ app/controllers/BalancesController.scala
@@ -4,6 +4,7 @@ import javax.inject.{Inject, Singleton}
 
 import models._
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import play.api._
 import play.api.libs.json._
 import play.api.mvc._
@@ -15,45 +16,58 @@ import play.api.libs.functional.syntax._
 
 @Singleton
 class BalancesController @Inject() (dao: BalancesDAO) extends Controller{
+  //val dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+  val dateFormat = "yyyy-MM-dd"
+
+  val jodaDateReads = Reads[DateTime](js =>
+    js.validate[String].map[DateTime](dtString =>
+      DateTime.parse(dtString, DateTimeFormat.forPattern(dateFormat))
+    )
+  )
+
+  val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(d: DateTime): JsValue = JsString(d.toString())
+  }
+
   implicit val depositReads: Reads[Deposit] = (
     (JsPath \ "accountNumber").read[Int] and
       (JsPath \ "value").read[Double] and
-      (JsPath \ "date").read[DateTime] and
+      (JsPath \ "date").read[DateTime](jodaDateReads) and
       (JsPath \ "description").read[String]
     )(Deposit.apply _)
 
   implicit val purchaseReads: Reads[Purchase] = (
     (JsPath \ "accountNumber").read[Int] and
       (JsPath \ "value").read[Double] and
-      (JsPath \ "date").read[DateTime] and
+      (JsPath \ "date").read[DateTime](jodaDateReads) and
       (JsPath \ "description").read[String]
     )(Purchase.apply _)
 
   implicit val withdrawalReads: Reads[Withdrawal] = (
     (JsPath \ "accountNumber").read[Int] and
       (JsPath \ "value").read[Double] and
-      (JsPath \ "date").read[DateTime] and
+      (JsPath \ "date").read[DateTime](jodaDateReads) and
       (JsPath \ "description").read[String]
     )(Withdrawal.apply _)
 
   implicit val depositWrites: Writes[Deposit] = (
     (JsPath \ "accountNumber").write[Int] and
       (JsPath \ "value").write[Double] and
-      (JsPath \ "date").write[DateTime] and
+      (JsPath \ "date").write[DateTime](jodaDateWrites) and
       (JsPath \ "description").write[String]
     )(unlift(Deposit.unapply))
 
   implicit val purchaseWrites: Writes[Purchase] = (
     (JsPath \ "accountNumber").write[Int] and
       (JsPath \ "value").write[Double] and
-      (JsPath \ "date").write[DateTime] and
+      (JsPath \ "date").write[DateTime](jodaDateWrites) and
       (JsPath \ "description").write[String]
     )(unlift(Purchase.unapply))
 
   implicit val withdrawalWrites: Writes[Withdrawal] = (
     (JsPath \ "accountNumber").write[Int] and
       (JsPath \ "value").write[Double] and
-      (JsPath \ "date").write[DateTime] and
+      (JsPath \ "date").write[DateTime](jodaDateWrites) and
       (JsPath \ "description").write[String]
     )(unlift(Withdrawal.unapply))
 
@@ -65,7 +79,7 @@ class BalancesController @Inject() (dao: BalancesDAO) extends Controller{
       },
       deposit =>{
         dao.addOperation(deposit)
-        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+deposit.accountNumber+"' saved.") ))
+        Ok(Json.obj("status" ->"OK", "message" -> ("Deposit on account: '"+deposit.accountNumber+"' saved.") ))
       }
     )
   }
@@ -78,7 +92,7 @@ class BalancesController @Inject() (dao: BalancesDAO) extends Controller{
       },
       purchase =>{
         dao.addOperation(purchase)
-        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+purchase.accountNumber+"' saved.") ))
+        Ok(Json.obj("status" ->"OK", "message" -> ("Purchase debbited from: '"+purchase.accountNumber+"' saved.") ))
       }
     )
   }
@@ -91,8 +105,67 @@ class BalancesController @Inject() (dao: BalancesDAO) extends Controller{
       },
       withdrawal =>{
         dao.addOperation(withdrawal)
-        Ok(Json.obj("status" ->"OK", "message" -> ("Place '"+withdrawal.accountNumber+"' saved.") ))
+        Ok(Json.obj("status" ->"OK", "message" -> ("Withdrawal from account '"+withdrawal.accountNumber+"' saved.") ))
       }
     )
+  }
+
+  def balance(accountNumber: Int) = Action {
+    val operationsOption = dao.getOperations(accountNumber)
+    var amount = 0.0
+    operationsOption match {
+      case Some(dates) =>{
+        dates.foreach(operations => {
+          operations._2.foreach(operation => operation match {
+            case Purchase(accountNumber, value, date, description) => amount -= value
+            case Deposit(accountNumber, value, date, description) => amount += value
+            case Withdrawal(accountNumber, value, date, description) => amount -= value
+          })
+        })
+
+        Logger.debug(dates.size.toString)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Balance; '"+amount+"' saved."), "balance" -> amount ))
+      }
+      case None =>{
+        BadRequest((Json.obj("status" ->"KO", "message" -> "The account doesn't exist or doesn't have operations")))
+      }
+    }
+
+  }
+
+  def statement(accountNumber: Int, from: String, to: String) = Action {
+    val fromDate = DateTime.parse(from, DateTimeFormat.forPattern(dateFormat))
+    val toDate = DateTime.parse(to, DateTimeFormat.forPattern(dateFormat))
+    var amount = 0.0
+
+    dao.getOperations(accountNumber) match {
+      case Some(dates) =>{
+        dates.foreach(operations => {
+          amount = getBalanceAmount(amount, operations)
+          if(checkRange(fromDate, toDate, operations)){
+
+          }
+        })
+      }
+      case None =>
+    }
+
+
+    Ok(Json.obj("status" ->"OK", "message" -> ("From/to; '"+toDate.toString()+"' saved.")))
+  }
+
+
+  private def getBalanceAmount(valAmount: Double, operations: (DateTime, List[Operation])): Double = {
+    var amount = valAmount
+    operations._2.foreach(operation => operation match {
+      case Purchase(accountNumber, value, date, description) => amount -= value
+      case Deposit(accountNumber, value, date, description) => amount += value
+      case Withdrawal(accountNumber, value, date, description) => amount -= value
+    })
+    amount
+  }
+
+  private def checkRange(fromDate: DateTime, toDate: DateTime, operations: (DateTime, List[Operation])): Boolean = {
+    (operations._1.isAfter(fromDate) || operations._1.isEqual(fromDate)) && (operations._1.isBefore(toDate) || operations._1.isEqual(toDate))
   }
 }
--- app/controllers/BalancesDAO.scala
+++ app/controllers/BalancesDAO.scala
@@ -2,6 +2,7 @@ package controllers
 
 import models.Operation
 import org.joda.time.DateTime
+import play.api.Logger
 
 import scala.collection.immutable.TreeMap
 
@@ -9,27 +10,47 @@ import scala.collection.immutable.TreeMap
   * Created by Dener on 18/04/2017.
   */
 class BalancesDAO {
-  var accountsMap = Map[Int, TreeMap[DateTime, Operation]]()
+  var accountsMap = Map[Int, TreeMap[DateTime, List[Operation]]]()
   implicit def ord: Ordering[DateTime] = Ordering.fromLessThan(_ isBefore _)
 
-  def addOperation(operation: Operation) : TreeMap[DateTime, Operation] = {
+  def addOperation(operation: Operation) : TreeMap[DateTime, List[Operation]] = {
 
     accountsMap.get(operation.accountNumber) match {
-      case Some(operations) => {
-        accountsMap = accountsMap - operation.accountNumber
-        val uOperation = operations +(operation.date -> operation)
-        accountsMap = accountsMap + (operation.accountNumber -> uOperation)
-        return uOperation
+      case Some(dates) => {
+//        Logger.debug("There are operations: " + operations.size.toString)
+//        accountsMap = accountsMap - operation.accountNumber
+//        val uOperation1 = TreeMap[DateTime, Operation](operation.date -> operation)
+//        val uOperation2 = operations ++ uOperation1
+//        accountsMap = accountsMap + (operation.accountNumber -> uOperation2)
+//        Logger.debug("There are operations: " + uOperation2.size.toString)
+//        return uOperation2
+        dates.get(operation.date) match {
+          case Some(date) =>{
+            val newDate = operation :: date
+            val newDates = dates + (operation.date -> newDate)
+            accountsMap = accountsMap + (operation.accountNumber -> newDates)
+            return newDates
+          }
+          case None =>{
+            val operations = List[Operation](operation)
+            val date = TreeMap[DateTime, List[Operation]](operation.date -> operations)
+            val newDates = dates ++ date
+            accountsMap = accountsMap + (operation.accountNumber -> newDates)
+            return dates
+          }
+        }
       }
       case None => {
-        val uOperation = TreeMap[DateTime, Operation](operation.date -> operation)
-        accountsMap = accountsMap + (operation.accountNumber -> uOperation)
-        return uOperation
+        Logger.debug("First operation")
+        val operations = List[Operation](operation)
+        val dates = TreeMap[DateTime, List[Operation]](operation.date -> operations)
+        accountsMap = accountsMap + (operation.accountNumber -> dates)
+        return dates
       }
     }
   }
 
-  def getOperations(accountNumber: Int) : Option[TreeMap[DateTime, Operation]] = {
+  def getOperations(accountNumber: Int) : Option[TreeMap[DateTime, List[Operation]]] = {
     accountsMap.get(accountNumber)
   }
 
